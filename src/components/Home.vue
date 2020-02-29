f2c3341fe89b6cd193110866372563bb33822b09
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2020-02-29 19:44:22 +0100

    Ajout de la page principale

---
+++ src/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container class="grey lighten-5">
+    <v-row class="mb-6" no-gutters>
+      <v-col>
+        <v-card class="pa-2" outlined tile>
+          <v-text-field label="Nom de l'exercice"></v-text-field>
+          <v-select v-model="lang" :items="languages" label="Langage" required></v-select>
+          <v-btn color="primary" depressed>
+            <v-icon>save</v-icon>
+            Sauvegarder
+          </v-btn>
+          <h2>Consignes</h2>
+          <v-textarea
+            name="input-7-1"
+            filled
+            auto-grow
+            value="Écrivez ici vos consignes."
+          ></v-textarea>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="pa-2" outlined tile>
+          <div id="editor" class="exercise-editor-ace-editor"></div>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ace from 'ace-builds/src-noconflict/ace'
+import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/mode-python'
+import 'ace-builds/webpack-resolver'
+
+export default {
+  name: 'Home',
+  data: () => ({
+    languages: ['Python'],
+    editor: null,
+    lang: 'python'
+  }),
+  mounted () {
+    this.editor = ace.edit('editor')
+    this.editor.setTheme('ace/theme/monokai')
+    this.editor.session.setMode(`ace/mode/${this.lang}`)
+    this.editor.setValue('Ne marche pas :(')
+  }
+
+}
+</script>
+
+<style>
+.exercise-editor-ace-editor {
+ position: relative;
+ height: 20rem;
+}
+</style>
