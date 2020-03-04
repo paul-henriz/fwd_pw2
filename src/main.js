96c7b517e46e977fc21202fac204a6306dbac784
Paul-Henri Zimmerlin <paulhenri.zimmerlin@gmail.com>
2020-03-04 09:22:45 +0100

    Ajout de Vuex

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-import createRouter from './router'
+import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from './store/index'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
 axios.defaults.withCredentials = true
 
-const state = {
-  user: {}
-}
-
 new Vue({
-  data: state,
-  router: createRouter(state),
+  store,
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -2,39 +2,36 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
-import axios from 'axios'
+import store from '../store/index'
 
 Vue.use(VueRouter)
 
-function createRouter (state) {
-  async function beforeEnter (to, from, next) {
-    try {
-      const { data: user } = await axios.get('http://localhost:3000/api/v1/me')
-      state.user = user
-      next()
-    } catch (err) {
-      console.log('err', err)
-      next('/login') // redirect to login if user is not authenticated
-    }
+async function beforeEnter (_to, _from, next) {
+  if (!store.getters['user/isAuthenticated']) {
+    await store.dispatch('user/fetchUser')
   }
-  const routes = [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      beforeEnter
-    },
-
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    }
-  ]
-
-  return new VueRouter({
-    routes
-  })
+  if (store.getters['user/isAuthenticated']) {
+    next()
+    return
+  }
+  next('/login')
 }
 
-export default createRouter
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    beforeEnter
+  },
+
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
+
+export default new VueRouter({
+  routes
+})
